5d3ccc3ed002c7a4f0682de2eb1b743b5ea72941
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2024-05-09 08:53:15 -0400

    did problem 13

--- Problem13/Problem13.cpp
+++ Problem13/Problem13.cpp
@@ -16,8 +16,7 @@ M             1000
 class Solution {
 public:
     int romanToInt(std::string s) {
-        std::vector<std::string> romanNums{ "I", "V", "X", "L", "C", "D", "M" };
-        std::vector<int> correspondingNums = { 1, 5, 10, 50, 100, 500, 1000 };
+
 
         int number = 0;
            
@@ -25,24 +24,50 @@ public:
         // If the number after is bigger, than the list is that number - current number
         //If second number is smaller, is added to current number
 
+        
         for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; i < romanNums.size(); j++) {
-                if (s[i] == romanNums[j][0]) {
-                    if (s[i + 1] == romanNums[j + 1][0]) { //if following number is bigger
-                        number = number + correspondingNums[i + 1] - correspondingNums[i];
-                    }
-                    else { //if following number is not bigger
-                        number = number + correspondingNums[i];
-                    }
+            if ((s.length() - 1) != i) {
+				std::cout << "We are not at the end of the string\n";
+                if (isAhead(s[i], s[i + 1])) {
+					std::cout << "Is ahead with numbers:" << s[i] << " and " << s[i + 1] << std::endl;
+					number += (RtN(s[i + 1]) - RtN(s[i]));
+					i++;
+				}
+				else {
+                    number += RtN(s[i]);
                 }
+			}
+			else {
+				number += RtN(s[i]);
+			}
+        }
+
+    return number;
+    }
+
+    int RtN(char roman) { //
+        std::vector<char> romanNums = { 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+        std::vector<int> correspondingNums = { 1, 5, 10, 50, 100, 500, 1000 };
+
+        for (int i = 0; i < romanNums.size(); i++) {
+            if (roman == romanNums[i]) {
+                return correspondingNums[i];
             }
         }
-        return number;
     }
+
+    bool isAhead(char firstNum, char secondNum) {
+        std::cout << "if " << firstNum << " is ahead of " << secondNum << "\n";
+        if (RtN(firstNum) < RtN(secondNum)) {
+            return true;
+        }
+        else { return false; }
+    }
+
 };
 
 int main()
 {
     Solution mySolution;
-    std::cout << mySolution.romanToInt("V") << std::endl;
+    std::cout << mySolution.romanToInt("VX") << std::endl;
 }
