c2d8eea556e220ef7fa7b3639f1e6d5acd1f5c08
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2025-01-15 12:16:41 -0500

    compleated 14-bad execution time

--- 14.longest-common-prefix.cpp
+++ 14.longest-common-prefix.cpp
@@ -8,7 +8,31 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        
+
+        string longestPrefix = strs[0];
+
+        for (int i = 1; i < strs.size(); i++) {
+            int prefLength = 0;
+            for (int j = 0; j < returnLengthSmaller(longestPrefix, strs[i]); j++) {
+                if(longestPrefix[j] == strs[i][j]) {
+                    prefLength++;
+                } else {
+                    j = returnLengthSmaller(longestPrefix, strs[i]);
+                }
+            }
+            if (prefLength != 0) {
+                longestPrefix = longestPrefix.substr(0, prefLength);
+            } else {
+                return "";
+            }
+            
+        }
+
+        return longestPrefix;
+    }
+
+    int returnLengthSmaller(string a, string b) {
+        return (a.size() < b.size()) ? a.size()  : b.size();
     }
 };
 // @lc code=end
---
+++ NOTES.md
@@ -0,0 +1,11 @@
+## Vector access
+access vectors like this: ```Name[i]```
+
+## Get length of vector
+name.size(); //returns the size of a list
+
+## Access charicters in a string
+name[i];
+
+## Get string substring
+name.substr(index, length)
