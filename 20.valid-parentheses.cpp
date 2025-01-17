2af49db0e5ee8548012f615febaf9e597691cbc7
DanTheEpicMan <98715915+DanTheEpicMan@users.noreply.github.com>
2025-01-16 20:34:14 -0500

    semi-working (test case 91)

--- 20.valid-parentheses.cpp
+++ 20.valid-parentheses.cpp
@@ -7,33 +7,91 @@
 // @lc code=start
 class Solution {
 public:
+
     bool isValid(string s) {
         /**Plan
-         * Find out that even
-         * Loop through string to find an end to index 0,
-         * then delete both
-         * reloop
+         * Get index 0
+         * Get opposite of index 0
+         * int b, s, c;
          * 
-         * RERUN:
-         * find index 0
-         * find opposite of index 0, if cant, return false
-         * loop until you find opposite
-         * delete 0 and found
+         * 
+         * 
+         * Examples:
+         * (  )  [  ]  {  }
+         * 1  0  1  0  1  0
+         * b  b  s  s  c  c
+         * (  [  ]  )
+         * 1  1  0  0
+         * b  s  s  b
+         * (  [  )  ]
+         * 1  1  0  0
+         * b  s  b  s
+         * (  ]
+         * 1  -1
+         * b  s
+         * (  {  {  }  }  [  ]  )
+         * 1  1  2  1  0  1  0  0
+         * b  c  c  c  c  s  s  b
+         * 
+         * Previous char
+         * if older position closes, goes down
+         * dont delete
+         * make list of open positions iterated over
+         * 
+         * find first close, 
+         * loop back until hit open, check for same type, fail/delete, -1 index
+         * 
+         * index 0-
+         * create a list of opens, 
+         * check if most recent open matches close
+         * delete from list of opens, string does not need to change
          */
+        std::vector<char> stack;
+        int i = 0;
+        if (s.size() % 2 == 1) {return false;}
         while (s.size() > 1) {
-           
+            std::cout << s[i] << std::endl;
+            if (s[i] == ")"[0] || s[i] == "]"[0] || s[i] == "}"[0]) {
+                std::cout << "ender" << std::endl;
+                if (stack.size() == 0 || flip(s[i]) != stack[stack.size()-1]) {
+                    return false;
+                }
+                std::cout << "flip: " << flip(s[i]) << "stack: " << stack[stack.size()-1] << std::endl;
+                s = removeIndexs(s, i-1, i);
+                std::cout << "Removed: " << s << std::endl;
+                i -= 1;
+                stack.erase( stack.begin() + stack.size() - 1);
+                std::cout << s << std::endl;
+            } else if (s[i] == "("[0] || s[i] == "["[0] || s[i] == "{"[0]) {
+                stack.push_back(s[i]);
+                i++;
+            } else {
+                return false;
+            }
+            std::cout << "end, i:" << i << std::endl;
 
         }
         return true;
+
+
     }
 
-    string flip(char s) {
-        if (s == "("[0]) {
-            return ")";
-        } else if (s == "["[0]) {
-            return "]";
+    /*
+    from 0 to index, from index + 1 to index2 - index1 - 1, to index2 + 1 to size-index2-1
+    a b c d e f g
+    0 1 2 3 4 5 6
+    */
+    string removeIndexs(string s, int in1, int in2) {
+        return s.substr(0, in1)  + s.substr(in1+1, in2-in1-1)  + s.substr(in2+1, s.size()-in2-1);
+    }
+
+    char flip(char s) {
+        if (s == ")"[0]) {
+            return "("[0];
+        } else if (s == "]"[0]) {
+            return "["[0];
         } else {
-            return "}";
+            return "{"[0];
         }
     }
 };
--- NOTES.md
+++ NOTES.md
@@ -9,6 +9,7 @@ name[i];
 
 ## Get string substring
 name.substr(index, length)// inclusive
+//length starts at self, ie, index, 1 = capture self, 0 = nothing
 
 ## Find string in string
 int index = name.find("what to find")
